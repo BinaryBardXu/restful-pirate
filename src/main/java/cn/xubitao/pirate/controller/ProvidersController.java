31e34b8296ec61e21e47c957997702b238cd81b5
xubitao <766191920@qq.com>
2016-01-01 16:27:09 +0800

    添加create逻辑,将其他方法的返回值由ProviderResource改为RestResource

--- src/main/java/cn/xubitao/pirate/controller/ProvidersController.java
+++ src/main/java/cn/xubitao/pirate/controller/ProvidersController.java
@@ -4,21 +4,17 @@ import cn.xubitao.pirate.assmbler.ProviderResourceAssembler;
 import cn.xubitao.pirate.assmbler.ProvidersResourceAssembler;
 import cn.xubitao.pirate.domain.Provider;
 import cn.xubitao.pirate.domain.Providers;
-import cn.xubitao.pirate.resource.BlankResource;
-import cn.xubitao.pirate.resource.ProviderResource;
+import cn.xubitao.pirate.resource.RestResource;
 import cn.xubitao.pirate.resource.ProvidersResource;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
 import java.sql.SQLException;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Created by xubitao on 12/27/15.
@@ -26,23 +22,23 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RestController
 @RequestMapping("/providers")
 public class ProvidersController {
-    @Resource
+    @javax.annotation.Resource
     Providers providers;
 
     @RequestMapping(method = RequestMethod.GET)
-    public HttpEntity<ProvidersResource> loadAll() throws SQLException {
+    public HttpEntity<RestResource> loadAll() throws SQLException {
         Providers providerList = providers.loadAll();
         ProvidersResourceAssembler providersResourceAssembler = new ProvidersResourceAssembler();
-        ProvidersResource providersResource = providersResourceAssembler.toResource(providerList);
-        return new ResponseEntity<ProvidersResource>(providersResource, HttpStatus.OK);
+        RestResource providersResource = providersResourceAssembler.toResource(providerList);
+        return new ResponseEntity<RestResource>(providersResource, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public HttpEntity<ProviderResource> findById(@PathVariable Integer id) throws SQLException {
+    public HttpEntity<RestResource> findById(@PathVariable Integer id) throws SQLException {
         Provider provider = providers.findById(id);
         ProviderResourceAssembler providerResourceAssembler = new ProviderResourceAssembler();
-        ProviderResource providerResource = providerResourceAssembler.toResource(provider);
-        return new ResponseEntity<ProviderResource>(providerResource, HttpStatus.OK);
+        RestResource providerResource = providerResourceAssembler.toResource(provider);
+        return new ResponseEntity<RestResource>(providerResource, provider == null ? HttpStatus.NOT_FOUND : HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
@@ -55,6 +51,14 @@ public class ProvidersController {
     public ResponseEntity deleteById(@PathVariable Integer id) throws SQLException {
         providers.deleteById(id);
         Link link = linkTo(ProvidersController.class).withRel("providers");
-        return new ResponseEntity(BlankResource.link(link), HttpStatus.OK);
+        return new ResponseEntity(RestResource.link(link), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity create(@RequestBody Provider provider) throws SQLException {
+        Provider savedProvider = providers.create(provider);
+        ProviderResourceAssembler providerResourceAssembler = new ProviderResourceAssembler();
+        RestResource providerResource = providerResourceAssembler.toResource(savedProvider);
+        return new ResponseEntity<RestResource>(providerResource, HttpStatus.OK);
     }
 }
