cf840071d3d744c6be5e72e91bc174bc457deb7c
xubitao <766191920@qq.com>
2016-01-01 11:39:09 +0800

    格式化代码

--- src/main/java/cn/xubitao/pirate/resource/ProviderResource.java
+++ src/main/java/cn/xubitao/pirate/resource/ProviderResource.java
@@ -12,19 +12,19 @@ public class ProviderResource extends ResourceSupport {
     private String name;
     private String version;
 
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
     }
 }
