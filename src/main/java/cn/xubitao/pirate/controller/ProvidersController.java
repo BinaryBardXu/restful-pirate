48457dc670c411ebd8e9dedc5746b8db02fcc774
xubitao <766191920@qq.com>
2016-01-01 15:06:30 +0800

    添加删除逻辑

--- src/main/java/cn/xubitao/pirate/controller/ProvidersController.java
+++ src/main/java/cn/xubitao/pirate/controller/ProvidersController.java
@@ -2,10 +2,13 @@ package cn.xubitao.pirate.controller;
 
 import cn.xubitao.pirate.assmbler.ProviderResourceAssembler;
 import cn.xubitao.pirate.assmbler.ProvidersResourceAssembler;
-import cn.xubitao.pirate.domain.Providers;
 import cn.xubitao.pirate.domain.Provider;
+import cn.xubitao.pirate.domain.Providers;
+import cn.xubitao.pirate.resource.BlankResource;
 import cn.xubitao.pirate.resource.ProviderResource;
 import cn.xubitao.pirate.resource.ProvidersResource;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +16,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.sql.SQLException;
-import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Created by xubitao on 12/27/15.
@@ -31,11 +36,25 @@ public class ProvidersController {
         ProvidersResource providersResource = providersResourceAssembler.toResource(providerList);
         return new ResponseEntity<ProvidersResource>(providersResource, HttpStatus.OK);
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public HttpEntity<ProviderResource> findById(@PathVariable Integer id) throws SQLException {
         Provider provider = providers.findById(id);
         ProviderResourceAssembler providerResourceAssembler = new ProviderResourceAssembler();
         ProviderResource providerResource = providerResourceAssembler.toResource(provider);
         return new ResponseEntity<ProviderResource>(providerResource, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity update(@RequestBody Provider provider, @PathVariable Integer id) throws SQLException {
+        providers.update(provider, id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteById(@PathVariable Integer id) throws SQLException {
+        providers.deleteById(id);
+        Link link = linkTo(ProvidersController.class).withRel("providers");
+        return new ResponseEntity(BlankResource.link(link), HttpStatus.OK);
     }
 }
