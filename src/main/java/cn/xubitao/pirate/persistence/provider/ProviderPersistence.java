3d31bfb25f0b4e219f1faa84cfab3697e9b2841d
xubitao <766191920@qq.com>
2016-01-02 15:30:25 +0800

    添加对多个字段联合查询的支持

--- src/main/java/cn/xubitao/pirate/persistence/provider/ProviderPersistence.java
+++ src/main/java/cn/xubitao/pirate/persistence/provider/ProviderPersistence.java
@@ -4,6 +4,8 @@ import cn.xubitao.pirate.domain.Provider;
 import cn.xubitao.pirate.domain.Providers;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xubitao on 12/25/15.
@@ -18,4 +20,6 @@ public interface ProviderPersistence {
     int update(Provider provider, Integer id) throws SQLException;
 
     int deleteById(Integer id) throws SQLException;
+
+    List<Provider> findByFieldValues(Map fieldValues) throws SQLException;
 }
