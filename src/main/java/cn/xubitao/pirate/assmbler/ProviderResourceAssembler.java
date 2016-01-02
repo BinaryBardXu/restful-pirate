a65624496338708f3502ae06223faada32e63ac9
xubitao <766191920@qq.com>
2016-01-02 14:03:12 +0800

    简化处理逻辑,细节进一步抽象后封装到其他类,继承于DolphinAssembler

--- src/main/java/cn/xubitao/pirate/assmbler/ProviderResourceAssembler.java
+++ src/main/java/cn/xubitao/pirate/assmbler/ProviderResourceAssembler.java
@@ -1,13 +1,11 @@
 package cn.xubitao.pirate.assmbler;
 
+import cn.xubitao.dolphin.foundation.assmbler.DolphinAssembler;
+import cn.xubitao.dolphin.foundation.resource.RestResource;
 import cn.xubitao.pirate.controller.ProvidersController;
 import cn.xubitao.pirate.domain.Provider;
-import cn.xubitao.pirate.resource.RestResource;
 import cn.xubitao.pirate.resource.ProviderResource;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
-
-import java.sql.SQLException;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -15,26 +13,24 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 /**
  * Created by xubitao on 12/30/15.
  */
-public class ProviderResourceAssembler extends ResourceAssemblerSupport<Provider, RestResource> {
+public class ProviderResourceAssembler extends DolphinAssembler {
+
     public ProviderResourceAssembler() {
         super(ProvidersController.class, RestResource.class);
     }
 
-    public RestResource toResource(Provider provider) {
+    public RestResource toRestResource(Object domain) throws Exception {
+        Provider provider = (Provider) domain;
         ProviderResource providerResource = new ProviderResource();
-        try {
-            Link providersLink = linkTo(ProvidersController.class).withRel("providers");
-            if (provider == null) {
-                return RestResource.link(providersLink);
-            }
-            providerResource.setName(provider.getName());
-            providerResource.setVersion(provider.getVersion());
-            providerResource.add(linkTo(methodOn(ProvidersController.class).findById(provider.getId())).withSelfRel());
-
-            providerResource.add(providersLink);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        Link providersLink = linkTo(ProvidersController.class).withRel("providers");
+        if (domain == null) {
+            return RestResource.link(providersLink);
         }
+        providerResource.setName(provider.getName());
+        providerResource.setVersion(provider.getVersion());
+        providerResource.add(linkTo(methodOn(ProvidersController.class).findById(provider.getId())).withSelfRel());
+
+        providerResource.add(providersLink);
         return providerResource;
     }
 }
--- src/main/java/cn/xubitao/pirate/assmbler/ProvidersResourceAssembler.java
+++ src/main/java/cn/xubitao/pirate/assmbler/ProvidersResourceAssembler.java
@@ -1,14 +1,12 @@
 package cn.xubitao.pirate.assmbler;
 
+import cn.xubitao.dolphin.foundation.assmbler.DolphinAssembler;
+import cn.xubitao.dolphin.foundation.resource.RestResource;
 import cn.xubitao.pirate.controller.ProvidersController;
-import cn.xubitao.pirate.domain.Provider;
 import cn.xubitao.pirate.domain.Providers;
 import cn.xubitao.pirate.resource.ProvidersResource;
-import cn.xubitao.pirate.resource.RestResource;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,34 +16,22 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 /**
  * Created by xubitao on 1/1/16.
  */
-public class ProvidersResourceAssembler extends ResourceAssemblerSupport<Providers, RestResource> {
+public class ProvidersResourceAssembler extends DolphinAssembler {
     public ProvidersResourceAssembler() {
         super(ProvidersController.class, RestResource.class);
     }
 
-    public RestResource toResource(Providers providers) {
+    public RestResource toRestResource(Object domain) throws Exception {
+        Providers providers = (Providers) domain;
         ProvidersResource providersResource = new ProvidersResource();
-        try {
-            Link providersLink = linkTo(ProvidersController.class).withRel("providers");
-            if (providers == null) {
-                return RestResource.link(providersLink);
-            }
-            List<RestResource> providerResources = buildProviderResources(providers);
-            providersResource.setProviders(providerResources);
-            providersResource.add(linkTo(methodOn(ProvidersController.class).loadAll()).withSelfRel());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return providersResource;
-    }
 
-    private List<RestResource> buildProviderResources(Providers providers) {
-        List<RestResource> providerResources = new ArrayList<RestResource>();
-        ProviderResourceAssembler providerResourceAssembler = new ProviderResourceAssembler();
-        for (Provider provider : providers.getProviders()) {
-            RestResource providerResource = providerResourceAssembler.toResource(provider);
-            providerResources.add(providerResource);
+        Link providersLink = linkTo(ProvidersController.class).withRel("providers");
+        if (providers == null) {
+            return RestResource.link(providersLink);
         }
-        return providerResources;
+        List<RestResource> providerResources = buildResources(providers.getProviders(), new ProviderResourceAssembler());
+        providersResource.setProviders(providerResources);
+        providersResource.add(linkTo(methodOn(ProvidersController.class).loadAll()).withSelfRel());
+        return providersResource;
     }
 }
