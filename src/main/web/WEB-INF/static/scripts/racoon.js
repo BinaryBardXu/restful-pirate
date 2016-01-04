f85e9f40e15e3107091db4c5b83b36c007eed1ce
xubitao <766191920@qq.com>
2016-01-05 07:09:18 +0800

    新增用于HTTP请求错误时的弹窗

--- src/main/web/WEB-INF/static/scripts/racoon.js
+++ src/main/web/WEB-INF/static/scripts/racoon.js
@@ -1,15 +1,49 @@
 /**
  * Created by xubitao on 1/2/16.
  */
+
+var host = "http://localhost:8080";
 var Racoon = {
     restful: function (_options) {
         $.ajax({
             type: _options.type || "GET",
             url: _options.url,
-            dataType: "json",
+            dataType: _options.dataType || "json",
+            async: _options.async || true,
             success: function (_data) {
+                toastr.clear();
+                toastr.success('成功!');
                 _options.success(_data);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                var httpErrorModel = $("#httpErrorModel");
+                if (httpErrorModel.html() == undefined) {
+                    loadErrorModel(XMLHttpRequest.responseJSON.message)
+                }
+                $('#errorModel').modal();
             }
         });
+    },
+    getLink: function (_links, _rel) {
+        for (var index in _links) {
+            var link = _links[index];
+            if (link.rel == _rel) {
+                return link.href;
+            }
+        }
     }
 };
+
+function loadErrorModel(_message) {
+    Racoon.restful({
+        url: host + "/static/html/errorModel.html",
+        dataType: "html",
+        async: false,
+        success: function (_data) {
+            var errorModel = $(_data);
+            errorModel.find("#errorMessage").html(_message);
+            $("body").append(errorModel);
+            $('#errorModel').modal();
+        }
+    })
+}
