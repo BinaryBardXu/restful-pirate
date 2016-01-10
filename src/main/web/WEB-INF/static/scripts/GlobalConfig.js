8da75b7c816266c513beb332309c556063d51659
xubitao <766191920@qq.com>
2016-01-10 14:30:05 +0800

    添加HttpType常量集

--- src/main/web/WEB-INF/static/scripts/GlobalConfig.js
+++ src/main/web/WEB-INF/static/scripts/GlobalConfig.js
@@ -1,6 +1,13 @@
 /**
  * Created by xubitao on 1/9/16.
  */
-var GlobalConfig={
-    entrance:"http://localhost:8080/providers"
+var GlobalConfig = {
+    entrance: "http://localhost:8080/providers"
+};
+
+var HttpType = {
+    POST: "POST",
+    PUT: "PUT",
+    DELETE: "DELETE",
+    GET: "GET"
 };
