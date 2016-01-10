3c6b6cac35f88db5d464933bc9167b0e87152233
xubitao <766191920@qq.com>
2016-01-10 15:42:22 +0800

    完成contract的修改功能

--- src/main/web/WEB-INF/static/scripts/contracts.js
+++ src/main/web/WEB-INF/static/scripts/contracts.js
@@ -5,21 +5,30 @@
 var Contracts = {
     init: function (_link) {
         Contracts.loadContracts(_link);
-        Contracts.initModal(_link);
-
+        Contracts.setValidator();
+        Contracts.bindContractCreateButton(_link);
     },
-    initModal: function (_link) {
+    bindContractCreateButton: function (_link) {
         $('#create-contract-button').click(function () {
+            $('#contract-modal-title').html("创建Contract");
             $('#create-contract-form').data('bootstrapValidator').resetForm(true);
-            $('#createContractsModel').modal();
+            Contracts.bindResetSaveButton(_link);
+            $('#createOrUpdateContractsModel').modal();
         });
         $('#contractsModel').modal();
+
+    },
+    bindResetSaveButton: function (_link) {
+        $('#contract-resetBtn').unbind();
         $('#contract-resetBtn').click(function () {
             $('#create-contract-form').data('bootstrapValidator').resetForm(true);
         });
-        $("#contract-create-button").click(function () {
-            Contracts.save(_link);
+        $("#contract-save-button").unbind();
+        $("#contract-save-button").click(function () {
+            Contracts.create(_link);
         });
+    },
+    setValidator: function () {
         $('#create-contract-form').bootstrapValidator({
             live: 'enabled',
             fields: {
@@ -77,7 +86,13 @@ var Contracts = {
             }
         });
     },
-    save: function (_link) {
+    create: function (_link) {
+        Contracts.save(_link, _link, HttpType.POST);
+    },
+    update: function (_link, _contractLink) {
+        Contracts.save(_link, _contractLink, HttpType.PUT);
+    },
+    save: function (_selfLink, _contractLink, _type) {
         var validResult = $('#create-contract-form').data('bootstrapValidator').validate().isValid();
         if (!validResult) {
             return;
@@ -88,13 +103,14 @@ var Contracts = {
         contract.request = $('#contract-request').val();
         contract.response = $('#contract-response').val();
         contract.desc = $('#contract-desc').val();
+
         Racoon.restful({
-            url: _link,
-            type: "POST",
+            url: _selfLink,
+            type: _type,
             data: contract,
             success: function () {
-                Contracts.init(_link);
-                $('#createContractsModel').modal('hide');
+                Contracts.loadContracts(_contractLink);
+                $('#createOrUpdateContractsModel').modal('hide');
             }
         });
     }
@@ -108,19 +124,42 @@ function openConfirmDeleteContractModal(_selfLink, _contractsLink) {
             type: 'DELETE',
             success: function () {
                 $('#confirmDeleteModel').modal('hide');
-                Contracts.init(_contractsLink);
+                Contracts.loadContracts(_contractsLink);
             }
         })
     });
     $('#confirmDeleteModel').modal();
+}
+function openContractUpdateModal(_selfLink, _contractLink) {
+    Racoon.restful({
+        url: _selfLink,
+        success: function (_data) {
+            var contract = _data;
+            $('#contract-modal-title').html("更新" + contract.name);
+
+            $('#contract-name').val(contract.name);
+            $('#contract-url').val(contract.url);
+            $('#contract-request').val(contract.request);
+            $('#contract-response').val(contract.response);
+            $('#contract-desc').val(contract.desc);
+
+            $("#contract-save-button"). unbind();
+            $("#contract-save-button").click(function () {
+                Contracts.update(_selfLink, _contractLink);
+            });
+            $('#createOrUpdateContractsModel').modal();
+        }
+    })
 }
 function contractsOptionsFormatter(_links) {
     var options = $("<div></div>");
     var span = $("<span class='button-dropdown' data-buttons='dropdown'></span>");
     var button = "<button class='button button-rounded'><i class='fa fa-bars'></i> 操作 </button>";
     var ul = $("<ul class='button-dropdown-list'></ul>");
+    var updateLink = $("<li><a class='pirate-link' onclick='openContractUpdateModal(\"" + Racoon.getLink(_links, "self") + "\",\"" + Racoon.getLink(_links, "contracts") + "\")'>更新</a></li>");
     var deleteLink = $("<li><a class='pirate-link' onclick='openConfirmDeleteContractModal(\"" + Racoon.getLink(_links, "self") + "\",\"" + Racoon.getLink(_links, "contracts") + "\")'>删除</a></li>"
     );
+    ul.append(updateLink);
     ul.append(deleteLink);
 
     span.append(button);
