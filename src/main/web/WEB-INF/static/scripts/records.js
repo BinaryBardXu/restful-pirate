9e84e32a16f03e64a60812f8f3d4a30f168e9f24
xubitao <766191920@qq.com>
2016-01-12 23:36:54 +0800

    添加loadMissedRecords方法

--- src/main/web/WEB-INF/static/scripts/records.js
+++ src/main/web/WEB-INF/static/scripts/records.js
@@ -15,8 +15,48 @@ var Records = {
                 var contracts = _data.contracts;
                 $('#records-table').bootstrapTable('load', contracts);
                 $('#records-table').bootstrapTable('hideLoading');
+                $('[data-toggle="popover"]').popover();
             }
         });
+    },
+    loadMissedRecords: function (_link) {
+        $('#records-table').bootstrapTable('showLoading');
+        Racoon.restful({
+            url: _link,
+            success: function (_data) {
+                var contracts = _data.contracts;
+                $('#missedRecords-table').bootstrapTable('load', contracts);
+                $('#missedRecords-table').bootstrapTable('hideLoading');
+                var recordsLink = Racoon.getLink(_data.links, "records");
+                Records.bindSearchEvent(recordsLink);
+                $('[data-toggle="popover"]').popover();
+            }
+        });
+    },
+    bindSearchEvent: function (_recordsLink) {
+        $('#missed-record-search-input').val("");
+        $('#missed-record-search-input').unbind();
+        $('#missed-record-search-input').bind("change", function () {
+            var consumerKey = $('#missed-record-search-input').val();
+            if (consumerKey == "consumerKey↵") return;
+            Records.searchByConsumerKey(_recordsLink, consumerKey);
+        });
+    },
+    searchByConsumerKey: function (_recordsLink, _consumerKey) {
+        $('#records-table').bootstrapTable('showLoading');
+        Racoon.restful({
+            url: _recordsLink,
+            data: {consumerKey: _consumerKey},
+            success: function (_data) {
+                var contracts = _data.contracts;
+                $('#missedRecords-table').bootstrapTable('load', contracts);
+                $('#missedRecords-table').bootstrapTable('hideLoading');
+            }
+        });
+    },
+    openMissedRecords: function (_link) {
+        Records.loadMissedRecords(_link);
+        $('#missedRecordsModel').modal();
     },
     refresh: function (_link) {
         $('#contracts-table').bootstrapTable('showLoading');
