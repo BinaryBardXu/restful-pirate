e63cb915456249b98fcfb6a5612efe73dbafa652
xubitao <766191920@qq.com>
2016-01-12 23:54:19 +0800

    构建links新增对missedRecords的构建

--- src/main/java/cn/xubitao/pirate/assmbler/provider/ProvidersResourceAssembler.java
+++ src/main/java/cn/xubitao/pirate/assmbler/provider/ProvidersResourceAssembler.java
@@ -3,6 +3,7 @@ package cn.xubitao.pirate.assmbler.provider;
 import cn.xubitao.dolphin.foundation.assmbler.DolphinAssembler;
 import cn.xubitao.dolphin.foundation.resource.RestResource;
 import cn.xubitao.pirate.controller.ProvidersController;
+import cn.xubitao.pirate.controller.RecordsController;
 import cn.xubitao.pirate.domain.provider.Providers;
 import cn.xubitao.pirate.resource.provider.ProvidersResource;
 import org.springframework.hateoas.Link;
@@ -17,6 +18,9 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
  * Created by xubitao on 1/1/16.
  */
 public class ProvidersResourceAssembler extends DolphinAssembler {
+
+    public static final int MISSED = 0;
+
     public ProvidersResourceAssembler() {
         super(ProvidersController.class, RestResource.class);
     }
@@ -33,6 +37,7 @@ public class ProvidersResourceAssembler extends DolphinAssembler {
         List<ResourceSupport> providerResources = buildResources(providers.getProviders(), new ProviderResourceAssembler(), pathVariables);
         providersResource.setProviders(providerResources);
         providersResource.add(linkTo(methodOn(ProvidersController.class).loadAll((String) pathVariables[0])).withSelfRel());
+        providersResource.add(linkTo(methodOn(RecordsController.class).loadAll(null, MISSED, null)).withRel("missedRecords"));
         return providersResource;
     }
 }
