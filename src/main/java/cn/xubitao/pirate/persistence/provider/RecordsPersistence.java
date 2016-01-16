1d56a77ee7ad48669211089c304c3263817a05d3
xubitao <766191920@qq.com>
2016-01-16 12:28:30 +0800

    将RecordPersistence修改为RecordsPersistence

---
+++ src/main/java/cn/xubitao/pirate/persistence/provider/RecordsPersistence.java
@@ -0,0 +1,15 @@
+package cn.xubitao.pirate.persistence.record;
+
+import cn.xubitao.pirate.domain.record.Record;
+import cn.xubitao.pirate.domain.record.Records;
+
+import java.sql.SQLException;
+
+/**
+ * Created by xubitao on 12/25/15.
+ */
+public interface RecordsPersistence {
+    Record create(Record record) throws SQLException;
+
+    Records loadAll(Integer contractId, Integer isHit, String consumerKey) throws SQLException;
+}
