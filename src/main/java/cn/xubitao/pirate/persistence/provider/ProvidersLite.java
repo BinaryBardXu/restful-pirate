bead3c0dd0ecfb0a773e2d92fd6c2816a0ff4223
xubitao <766191920@qq.com>
2016-01-16 12:29:57 +0800

    将ProviderLite修改为ProvidersLite

--- src/main/java/cn/xubitao/pirate/persistence/provider/ProvidersLite.java
+++ src/main/java/cn/xubitao/pirate/persistence/provider/ProvidersLite.java
@@ -17,49 +17,49 @@ import java.util.Map;
  * Created by xubitao on 12/25/15.
  */
 @Service
-public class ProviderLite implements ProviderPersistence {
+public class ProvidersLite implements ProvidersPersistence {
     @Resource
     private Dolphin dolphin;
-    private Dao<Provider, Integer> projectDAO;
+    private Dao<Provider, Integer> providerDAO;
 
     public Provider create(Provider provider) throws SQLException {
-        getProjectDAO().create(provider);
+        getProviderDAO().create(provider);
         return provider;
     }
 
     public Provider findById(Integer id) throws SQLException {
         Map conditions = ImmutableMap.of("id", id, "deleteStatus", 0);
-        List<Provider> result = getProjectDAO().queryForFieldValues(conditions);
+        List<Provider> result = getProviderDAO().queryForFieldValues(conditions);
         return result.size() > 0 ? result.get(0) : null;
     }
 
     public List<Provider> findByConditions(Map conditions) throws SQLException {
-        return getProjectDAO().queryForFieldValues(conditions);
+        return getProviderDAO().queryForFieldValues(conditions);
     }
 
     public Providers loadAll(String keyword) throws SQLException {
         keyword = keyword == null ? "%%" : "%" + keyword + "%";
-        QueryBuilder<Provider, Integer> queryBuilder = getProjectDAO().queryBuilder();
+        QueryBuilder<Provider, Integer> queryBuilder = getProviderDAO().queryBuilder();
         queryBuilder.where().like("name", keyword).or().like("consumerKey", keyword).and().eq("deleteStatus", 0);
 
         Providers providers = new Providers();
-        providers.setProviders(queryBuilder.query());
+        providers.setProviders(queryBuilder.limit(20).query());
         return providers;
     }
 
     public int update(Provider provider, Integer id) throws SQLException {
         provider.setId(id);
-        return getProjectDAO().update(provider);
+        return getProviderDAO().update(provider);
     }
 
     public int deleteById(Integer id) throws SQLException {
-        return getProjectDAO().executeRawNoArgs("update provider set deleteStatus=1 where id=" + id);
+        return getProviderDAO().executeRawNoArgs("update provider set deleteStatus=1 where id=" + id);
     }
 
-    public Dao<Provider, Integer> getProjectDAO() {
-        if (projectDAO == null) {
-            projectDAO = dolphin.lite(Provider.class);
+    public Dao<Provider, Integer> getProviderDAO() {
+        if (providerDAO == null) {
+            providerDAO = dolphin.lite(Provider.class);
         }
-        return projectDAO;
+        return providerDAO;
     }
 }
