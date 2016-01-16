f3c98e2100d80973d78abcf21883e258becc8ad6
xubitao <766191920@qq.com>
2016-01-16 12:31:21 +0800

    将RecordPersistence修改为RecordsPersistence

--- src/main/java/cn/xubitao/pirate/persistence/record/RecordsPersistence.java
+++ src/main/java/cn/xubitao/pirate/persistence/record/RecordsPersistence.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 /**
  * Created by xubitao on 12/25/15.
  */
-public interface RecordPersistence {
+public interface RecordsPersistence {
     Record create(Record record) throws SQLException;
 
     Records loadAll(Integer contractId, Integer isHit, String consumerKey) throws SQLException;
