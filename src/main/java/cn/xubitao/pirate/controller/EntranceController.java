4616f54e5ef7d7de5ba222a007103d43945064b7
xubitao <766191920@qq.com>
2016-01-16 12:09:54 +0800

    添加EntranceController

---
+++ src/main/java/cn/xubitao/pirate/controller/EntranceController.java
@@ -0,0 +1,22 @@
+package cn.xubitao.pirate.controller;
+
+import cn.xubitao.dolphin.foundation.response.Response;
+import cn.xubitao.pirate.assmbler.EntranceEntranceAssembler;
+import cn.xubitao.pirate.resource.EntranceResource;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xubitao on 12/27/15.
+ */
+@RestController
+@RequestMapping(value = "/entrance")
+public class EntranceController {
+    @RequestMapping(method = RequestMethod.GET)
+    public HttpEntity<ResourceSupport> enter() throws Exception {
+        return Response.build(new EntranceResource(), new EntranceEntranceAssembler());
+    }
+}
