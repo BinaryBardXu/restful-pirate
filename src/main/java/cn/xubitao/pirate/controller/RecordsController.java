11f71807b29eb1c7f6e67caaca21e7a45465df4c
xubitao <766191920@qq.com>
2016-01-23 10:58:41 +0800

    loadAll方法的参数改为Map

--- src/main/java/cn/xubitao/pirate/controller/RecordsController.java
+++ src/main/java/cn/xubitao/pirate/controller/RecordsController.java
@@ -2,6 +2,7 @@ package cn.xubitao.pirate.controller;
 
 import cn.xubitao.dolphin.foundation.resource.RestResource;
 import cn.xubitao.dolphin.foundation.response.Response;
+import cn.xubitao.dolphin.foundation.tools.Maps;
 import cn.xubitao.pirate.assmbler.record.RecordResourceAssembler;
 import cn.xubitao.pirate.assmbler.record.RecordsResourceAssembler;
 import cn.xubitao.pirate.domain.record.Record;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * Created by xubitao on 12/27/15.
@@ -23,7 +25,8 @@ public class RecordsController {
 
     @RequestMapping(method = RequestMethod.GET)
     public HttpEntity<ResourceSupport> loadAll(@RequestParam(required = false) Integer contractId, @RequestParam(required = false) Integer isHit, @RequestParam(required = false) String consumerKey) throws Exception {
-        Records recordList = records.loadAll(contractId, isHit, consumerKey);
+        Map conditions = Maps.build("contractId", contractId, "isHit", isHit, "consumerKey", consumerKey);
+        Records recordList = records.loadAll(conditions);
         return Response.build(recordList, new RecordsResourceAssembler(), contractId, isHit, consumerKey);
     }
 
