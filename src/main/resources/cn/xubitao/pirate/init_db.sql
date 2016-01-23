0e6d17a8ac5648a48c9867bd93a9171713cf72e7
xubitao <766191920@qq.com>
2016-01-23 11:28:00 +0800

    添加初始化mysql的语句

--- src/main/resources/cn/xubitao/pirate/init_db.sql
+++ src/main/resources/cn/xubitao/pirate/init_db.sql
@@ -1,3 +1,32 @@
--- noinspection SqlDialectInspectionForFile
-create table student(name varchar(10), age smallint);
-create table project(name varchar(50), version VARCHAR (50));
+CREATE TABLE contract
+(
+    id INT(11) PRIMARY KEY NOT NULL,
+    response TEXT,
+    request TEXT,
+    name VARCHAR(50),
+    providerId INT(11) NOT NULL,
+    description TEXT,
+    deleteStatus INT(11) DEFAULT '0',
+    server VARCHAR(200) NOT NULL,
+    excludeFields VARCHAR(200)
+);
+CREATE TABLE provider
+(
+    id INT(11) PRIMARY KEY NOT NULL,
+    name VARCHAR(50),
+    version VARCHAR(50),
+    deleteStatus INT(11) DEFAULT '0',
+    consumerKey VARCHAR(50) NOT NULL
+);
+CREATE TABLE record
+(
+    id INT(11) PRIMARY KEY NOT NULL,
+    request TEXT NOT NULL,
+    response TEXT,
+    contractId INT(11) NOT NULL,
+    deleteStatus INT(11) NOT NULL,
+    isHit INT(11) NOT NULL,
+    createTime VARCHAR(50),
+    consumerKey VARCHAR(50),
+    server VARCHAR(200)
+);
