65c8ae78635c1cb631753fad095db772ffd04cf0
xubitao <766191920@qq.com>
2016-01-23 11:01:54 +0800

    删除SQLLite的注解

--- src/main/java/cn/xubitao/pirate/domain/record/Record.java
+++ src/main/java/cn/xubitao/pirate/domain/record/Record.java
@@ -1,30 +1,17 @@
 package cn.xubitao.pirate.domain.record;
 
-import com.j256.ormlite.field.DatabaseField;
-import com.j256.ormlite.table.DatabaseTable;
-
 /**
  * Created by xubitao on 12/25/15.
  */
-@DatabaseTable(tableName = "record")
 public class Record {
-    @DatabaseField(generatedId = true)
     private Integer id;
-    @DatabaseField(canBeNull = false)
     private String server;
-    @DatabaseField(canBeNull = false)
     private String request;
-    @DatabaseField
     private String response;
-    @DatabaseField(canBeNull = false)
     private String consumerKey;
-    @DatabaseField(canBeNull = false)
     private Integer contractId;
-    @DatabaseField(canBeNull = false, defaultValue = "0")
     private Integer isHit;
-    @DatabaseField(defaultValue = "0")
     private Integer deleteStatus;
-    @DatabaseField
     private String createTime;
 
     public Integer getId() {
