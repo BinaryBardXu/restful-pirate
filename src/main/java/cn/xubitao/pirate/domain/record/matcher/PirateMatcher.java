a57835dedd9b2290607fd552ae13bb7f6d3c4c6b
xubitao <766191920@qq.com>
2016-01-28 23:38:01 +0800

    修改@Service为@Service("pirateMatcher")

--- src/main/java/cn/xubitao/pirate/domain/record/matcher/PirateMatcher.java
+++ src/main/java/cn/xubitao/pirate/domain/record/matcher/PirateMatcher.java
@@ -6,12 +6,14 @@ import cn.xubitao.pirate.domain.record.Record;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by 15031046 on 2016/1/12.
+ * Created by xubitao on 2016/1/12.
  */
-@Service
+@Service("pirateMatcher")
 public class PirateMatcher implements Matcher {
     public boolean match(Contract contract, Record record) {
-        if (!contract.getServer().equals(record.getServer())) return false;
+        if (!contract.getServer().equals(record.getServer())) {
+            return false;
+        }
         return People.compare(contract.getRequest(), record.getRequest(), contract.getExcludeFields());
     }
 }
