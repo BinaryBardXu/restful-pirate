cb3fb5c630dcb33aec9388253420bd3fcdee585f
xubitao <766191920@qq.com>
2016-01-28 23:28:52 +0800

    添加对stop-ajax-button的单击事件

--- src/main/resources/static/scripts/entrance.js
+++ src/main/resources/static/scripts/entrance.js
@@ -7,11 +7,15 @@ var Entrance = {
         Racoon.restful({
             url: GlobalConfig.entrance,
             success: function (_data) {
-                var links = _data.links;
+                var links = _data._links;
                 Providers.init(Racoon.getLink(links, "providers"));
                 Records.init(Racoon.getLink(links, "missedRecords"));
                 Statistics.init(Racoon.getLink(links, "statistic"));
             }
+        });
+        $("#stop-ajax-button").click(function () {
+            Racoon.currentRequest.abort();
+            $('#processing').hide();
         })
     }
 };
