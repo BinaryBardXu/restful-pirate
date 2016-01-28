3b7988b31e6a080c5956d7694f0663257fdc7337
xubitao <766191920@qq.com>
2016-01-28 23:21:12 +0800

    添加Application,作为程序的启动入口

---
+++ src/main/java/cn/xubitao/pirate/Application.java
@@ -0,0 +1,14 @@
+package cn.xubitao.pirate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xubitao on 12/27/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
