bd4ce8b38950c6974b1b73d7a50da4f4ad505361
xubitao <766191920@qq.com>
2016-01-28 23:55:29 +0800

    设置delete Status的值默认为0

--- src/main/resources/cn/xubitao/pirate/init_db.sql
+++ src/main/resources/cn/xubitao/pirate/init_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE contract
     name VARCHAR(50),
     providerId INT(11) NOT NULL,
     description TEXT,
-    deleteStatus INT(11) DEFAULT '0',
+    deleteStatus INT(11) DEFAULT '0' NOT NULL,
     server VARCHAR(200) NOT NULL,
     excludeFields VARCHAR(200)
 );
@@ -15,7 +15,7 @@ CREATE TABLE provider
     id INT(11) PRIMARY KEY NOT NULL,
     name VARCHAR(50),
     version VARCHAR(50),
-    deleteStatus INT(11) DEFAULT '0',
+    deleteStatus INT(11) DEFAULT '0' NOT NULL,
     consumerKey VARCHAR(50) NOT NULL
 );
 CREATE TABLE record
@@ -24,7 +24,7 @@ CREATE TABLE record
     request TEXT NOT NULL,
     response TEXT,
     contractId INT(11) NOT NULL,
-    deleteStatus INT(11) NOT NULL,
+    deleteStatus INT(11) DEFAULT '0' NOT NULL,
     isHit INT(11) NOT NULL,
     createTime VARCHAR(50),
     consumerKey VARCHAR(50),
