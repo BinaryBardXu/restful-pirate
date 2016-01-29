1a31e9ee08cb09ecbeb7ba4970e2a886087b645f
xubitao <766191920@qq.com>
2016-01-29 21:19:27 +0800

    实现records\hitRecords\missedRecords的统计逻辑

--- src/main/java/cn/xubitao/pirate/domain/statistic/Statistic.java
+++ src/main/java/cn/xubitao/pirate/domain/statistic/Statistic.java
@@ -1,10 +1,17 @@
 package cn.xubitao.pirate.domain.statistic;
 
+import cn.xubitao.dolphin.foundation.date.DateStyle;
+import cn.xubitao.dolphin.foundation.date.DateUtil;
+import cn.xubitao.dolphin.foundation.tools.Maps;
+import cn.xubitao.pirate.domain.record.Record;
 import cn.xubitao.pirate.persistence.statistic.StatisticPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xubitao on 2016/1/15.
@@ -20,9 +27,9 @@ public class Statistic {
     private long recordsCount;
     private long hitRecordsCount;
     private long missedRecordsCount;
-    private List<Integer> records;
-    private List<Integer> hitRecords;
-    private List<Integer> missedRecords;
+    private List<Map> records;
+    private List<Map> hitRecords;
+    private List<Map> missedRecords;
 
     public long getProviderCount() {
         return providerCount;
@@ -40,27 +47,27 @@ public class Statistic {
         this.contractsCount = contractsCount;
     }
 
-    public List<Integer> getRecords() {
+    public List<Map> getRecords() {
         return records;
     }
 
-    public void setRecords(List<Integer> records) {
+    public void setRecords(List<Map> records) {
         this.records = records;
     }
 
-    public List<Integer> getHitRecords() {
+    public List<Map> getHitRecords() {
         return hitRecords;
     }
 
-    public void setHitRecords(List<Integer> hitRecords) {
+    public void setHitRecords(List<Map> hitRecords) {
         this.hitRecords = hitRecords;
     }
 
-    public List<Integer> getMissedRecords() {
+    public List<Map> getMissedRecords() {
         return missedRecords;
     }
 
-    public void setMissedRecords(List<Integer> missedRecords) {
+    public void setMissedRecords(List<Map> missedRecords) {
         this.missedRecords = missedRecords;
     }
 
@@ -88,12 +95,55 @@ public class Statistic {
         this.missedRecordsCount = missedRecordsCount;
     }
 
-    public Statistic load() {
+    public Statistic load() throws Exception {
         this.providerCount = statisticPersistence.countProviders();
         this.contractsCount = statisticPersistence.countContracts();
         this.recordsCount = statisticPersistence.countRecords(null);
-        this.hitRecordsCount = statisticPersistence.countRecords(IS_HIT);
-        this.missedRecordsCount = statisticPersistence.countRecords(MISSED);
+        Record record = new Record();
+        record.setIsHit(IS_HIT);
+        this.hitRecordsCount = statisticPersistence.countRecords(record);
+        record.setIsHit(MISSED);
+        this.missedRecordsCount = statisticPersistence.countRecords(record);
+
+        statisticTrend();
         return this;
+    }
+
+    private void statisticTrend() throws Exception {
+        records = new ArrayList<Map>();
+        hitRecords = new ArrayList<Map>();
+        missedRecords = new ArrayList<Map>();
+        for (int i = 30; i >= 0; i--) {
+            Calendar currentDate = Calendar.getInstance();
+            currentDate.add(Calendar.DAY_OF_YEAR, -i);
+            Record record = new Record();
+            record.setCreateTime(DateUtil.DateToString(currentDate.getTime(), DateStyle.YYYY_MM_DD));
+
+            statisticAll(record);
+
+            statisticHit(record);
+
+            statisticMissed(record);
+        }
+    }
+
+    private void statisticMissed(Record record) throws Exception {
+        record.setIsHit(MISSED);
+        Integer missedRecordsCount = statisticPersistence.countRecords(record);
+        Map missedCountMap = Maps.build("date", record.getCreateTime(), "count", missedRecordsCount);
+        missedRecords.add(missedCountMap);
+    }
+
+    private void statisticHit(Record record) throws Exception {
+        record.setIsHit(IS_HIT);
+        Integer hitCount = statisticPersistence.countRecords(record);
+        Map hitCountMap = Maps.build("date", record.getCreateTime(), "count", hitCount);
+        hitRecords.add(hitCountMap);
+    }
+
+    private void statisticAll(Record record) throws Exception {
+        Integer recordsCount = statisticPersistence.countRecords(record);
+        Map recordCountMap = Maps.build("date", record.getCreateTime(), "count", recordsCount);
+        records.add(recordCountMap);
     }
 }
