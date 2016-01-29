172e629d59a3c44c1e34c7e85bc8c76cf0303e7c
xubitao <766191920@qq.com>
2016-01-29 21:03:17 +0800

    loadData时不再等待1秒钟.移除之前的演示数据,换成真实的处理逻辑.

--- src/main/resources/static/scripts/statistics.js
+++ src/main/resources/static/scripts/statistics.js
@@ -14,15 +14,13 @@ var Statistics = {
         });
     },
     loadData: function (_link) {
-        setTimeout(function () {
-            Racoon.restful({
-                url: _link,
-                success: function (_data) {
-                    Statistics.displayItems(_data);
-                    Statistics.displayCharts(_data);
-                }
-            })
-        }, 1000);
+        Racoon.restful({
+            url: _link,
+            success: function (_data) {
+                Statistics.displayItems(_data);
+                Statistics.displayCharts(_data);
+            }
+        })
     },
     displayItems: function (_data) {
         $("#statistics-providers-count").html(_data.providersCount);
@@ -41,13 +39,10 @@ var Statistics = {
 
             },
             title: {
-                text: '最近一周记录情况'
+                text: '最近一个月记录情况'
             },
             xAxis: {
-                type: 'datetime',
-                labels: {
-                    overflow: 'justify'
-                }
+                categories: Statistics.buildCategoriesData(_data)
             },
             yAxis: {
                 title: {
@@ -62,46 +57,48 @@ var Statistics = {
             tooltip: {
                 valueSuffix: ' 次数'
             },
-            plotOptions: {
-                spline: {
-                    lineWidth: 4,
-                    states: {
-                        hover: {
-                            lineWidth: 5
-                        }
-                    },
-                    marker: {
-                        enabled: false
-                    },
-                    pointInterval: 3600000, // one hour
-                    pointStart: Date.UTC(2009, 9, 6, 0, 0, 0)
-                }
-            },
             series: [{
                 name: '总数',
-                data: [4.3, 5.1, 4.3, 5.2, 5.4, 4.7, 3.5, 4.1, 5.6, 7.4, 6.9, 7.1,
-                    7.9, 7.9, 7.5, 6.7, 7.7, 7.7, 7.4, 7.0, 7.1, 5.8, 5.9, 7.4,
-                    8.2, 8.5, 9.4, 8.1, 10.9, 10.4, 10.9, 12.4, 12.1, 9.5, 7.5,
-                    7.1, 7.5, 8.1, 6.8, 3.4, 2.1, 1.9, 2.8, 2.9, 1.3, 4.4, 4.2,
-                    3.0, 3.0],
+                data: Statistics.buildSeriesData(_data),
                 color: '#563D7C'
 
             }, {
                 name: '命中',
-                data: [0.1, 0.5, 0.9, 0.3, 0.0, 0.0, 9.2, 0.9, 0.1, 0.0, 0.3, 0.0,
-                    0.0, 12.4, 0.0, 0.1, 0.8, 0.0, 0.9, 10.0, 0.8, 0.3, 0.2, 0.9,
-                    0.0, 0.6, 1.2, 1.7, 0.6, 5.9, 4.1, 8.6, 3.7, 3.9, 1.7, 8.3,
-                    3.0, 3.3, 4.8, 5.4, 4.8, 5.0, 3.2, 2.5, 0.9, 5.4, 0.3, 0.5, 0.4],
+                data: Statistics.buildHitSeriesData(_data),
                 color: 'green'
             }, {
                 name: '未命中',
-                data: [0.0, 0.0, 0.0, 0.0, 9.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.3, 0.0,
-                    0.0, 0.4, 0.0, 0.1, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5.0,
-                    0.0, 0.6, 1.2, 1.7, 0.7, 2.9, 4.1, 2.6, 3.7, 3.9, 1.7, 2.3,
-                    3.0, 3.3, 4.8, 5.0, 4.8, 5.0, 3.2, 2.0, 0.9, 0.4, 0.3, 0.5, 0.4]
-                ,
+                data: Statistics.buildMissedSeriesData(_data),
                 color: 'red'
             }]
         });
+    },
+    buildSeriesData: function (_data) {
+        var seriesRecordsData = [];
+        for (var index in _data.records) {
+            seriesRecordsData.push(_data.records[index].count);
+        }
+        return seriesRecordsData;
+    },
+    buildHitSeriesData: function (_data) {
+        var seriesRecordsData = [];
+        for (var index in _data.hitRecords) {
+            seriesRecordsData.push(_data.hitRecords[index].count);
+        }
+        return seriesRecordsData;
+    },
+    buildMissedSeriesData: function (_data) {
+        var seriesRecordsData = [];
+        for (var index in _data.missedRecords) {
+            seriesRecordsData.push(_data.missedRecords[index].count);
+        }
+        return seriesRecordsData;
+    },
+    buildCategoriesData: function (_data) {
+        var categoriesData = [];
+        for (var index in _data.records) {
+            categoriesData.push(_data.records[index].date);
+        }
+        return categoriesData;
     }
 };
