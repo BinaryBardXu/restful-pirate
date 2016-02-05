52fc432da209173cde86a0858b267649e9043a67
xubitao <tao@xubitao.cn>
2016-02-05 16:33:25 +0800

    创建数据库时主键增加 auto_increment

--- src/main/resources/cn/xubitao/pirate/init_db.sql
+++ src/main/resources/cn/xubitao/pirate/init_db.sql
@@ -1,30 +1,31 @@
 CREATE TABLE contract
 (
-    id INT(11) PRIMARY KEY NOT NULL,
+    id INT(11) PRIMARY KEY NOT NULL auto_increment,
     response TEXT,
     request TEXT,
     name VARCHAR(50),
     providerId INT(11) NOT NULL,
     description TEXT,
-    deleteStatus INT(11) DEFAULT '0' NOT NULL,
+    deleteStatus INT(11) DEFAULT '0',
     server VARCHAR(200) NOT NULL,
     excludeFields VARCHAR(200)
 );
 CREATE TABLE provider
 (
-    id INT(11) PRIMARY KEY NOT NULL,
+    id INT(11) PRIMARY KEY NOT NULL auto_increment,
     name VARCHAR(50),
     version VARCHAR(50),
-    deleteStatus INT(11) DEFAULT '0' NOT NULL
-    );
+    deleteStatus INT(11) DEFAULT '0'
+);
 CREATE TABLE record
 (
-    id INT(11) PRIMARY KEY NOT NULL,
+    id INT(11) PRIMARY KEY NOT NULL auto_increment,
     request TEXT NOT NULL,
     response TEXT,
     contractId INT(11) NOT NULL,
     deleteStatus INT(11) DEFAULT '0' NOT NULL,
     isHit INT(11) NOT NULL,
     createTime VARCHAR(50),
+    name VARCHAR(50),
     server VARCHAR(200)
 );
